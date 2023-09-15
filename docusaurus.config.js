a691ad09a79357a79193d2a9c3146175563fa704
Dev Chechi <devmchechi@gmail.com>
2023-09-15 19:01:57 -0400

    main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vaultdbai/vaultdb-docs/tree/master/docs/',
+            'https://github.com/vaultdbai/vaultdb-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/vaultdbai/vaultdb-docs/tree/master/docs/',
+            'https://github.com/vaultdbai/vaultdb-docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
